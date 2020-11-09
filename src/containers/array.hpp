a27bd556c98e3465d07205b6ac687e41afc41f3d
Kara <lh0xfb@gmail.com>
2020-11-08 22:51:26 -0800

    containers

--- src/containers/array.hpp
+++ src/containers/array.hpp
@@ -2,7 +2,9 @@
 
 #include "common/macro.h"
 #include "allocator/allocator.h"
+#include "containers/sort.hpp"
 #include <string.h>
+#include <new>
 
 template<typename T>
 class Array final
@@ -11,20 +13,22 @@ private:
     T* m_ptr;
     i32 m_length;
     EAlloc m_allocator;
+
 public:
+    bool InRange(i32 i) const { return (u32)i < (u32)m_length; }
+    EAlloc AllocType() const { return m_allocator; }
 
     i32 size() const { return m_length; }
-    EAlloc AllocType() const { return m_allocator; }
     T* begin() { return m_ptr; }
     T* end() { return m_ptr + m_length; }
     const T* begin() const { return m_ptr; }
     const T* end() const { return m_ptr + m_length; }
-    T& front() { ASSERT(m_length > 0); return m_ptr[0]; }
-    const T& front() const { ASSERT(m_length > 0); return m_ptr[0]; }
-    T& back() { ASSERT(m_length > 0); return m_ptr[m_length - 1]; }
-    const T& back() const { ASSERT(m_length > 0); return m_ptr[m_length - 1]; }
-    T& operator[](i32 i) { ASSERT((u32)i < (u32)m_length); return m_ptr[i]; }
-    const T& operator[](i32 i) const { ASSERT((u32)i < (u32)m_length); return m_ptr[i]; }
+    T& front() { ASSERT(InRange(0)); return m_ptr[0]; }
+    const T& front() const { ASSERT(InRange(0)); return m_ptr[0]; }
+    T& back() { ASSERT(InRange(0)); return m_ptr[m_length - 1]; }
+    const T& back() const { ASSERT(InRange(0)); return m_ptr[m_length - 1]; }
+    T& operator[](i32 i) { ASSERT(InRange(i)); return m_ptr[i]; }
+    const T& operator[](i32 i) const { ASSERT(InRange(i)); return m_ptr[i]; }
 
     explicit Array()
     {
@@ -55,13 +59,30 @@ public:
 
     explicit Array(Array&& other)
     {
-        memcpy(this, &other, sizeof(*this));
-        memset(&other, 0, sizeof(*this));
+        m_ptr = other.m_ptr;
+        m_length = other.m_length;
+        m_allocator = other.m_allocator;
+        other.m_ptr = NULL;
+        other.m_length = 0;
+    }
+    Array& operator=(Array&& other)
+    {
+        Reset();
+        m_ptr = other.m_ptr;
+        m_length = other.m_length;
+        m_allocator = other.m_allocator;
+        other.m_ptr = NULL;
+        other.m_length = 0;
+        return *this;
     }
 
     explicit Array(const Array& other)
     {
-        const i32 length = other.size();
+        m_ptr = NULL;
+        m_length = 0;
+        m_allocator = other.m_allocator;
+
+        const i32 length = other.m_length;
         Reserve(length);
         m_length = length;
         T *const pim_noalias dst = m_ptr;
@@ -71,15 +92,6 @@ public:
             new (dst + i) T(src[i]);
         }
     }
-
-    Array& operator=(Array&& other)
-    {
-        Reset();
-        memcpy(this, &other, sizeof(*this));
-        memset(&other, 0, sizeof(*this));
-        return *this;
-    }
-
     Array& operator=(const Array& other)
     {
         const i32 length = other.size();
@@ -98,8 +110,17 @@ public:
     void Pop()
     {
         ASSERT(m_length > 0);
-        i32 index = --m_length;
-        m_ptr[index].~T();
+        i32 b = --m_length;
+        m_ptr[b].~T();
+    }
+
+    void Remove(i32 index)
+    {
+        ASSERT(InRange(index));
+        T *const pim_noalias ptr = m_ptr;
+        i32 b = --m_length;
+        ptr[i].~T();
+        memcpy(ptr + i, ptr + b, sizeof(T));
     }
 
     void Clear()
@@ -122,19 +143,78 @@ public:
 
     void Reserve(i32 capacity)
     {
+        ASSERT(capacity >= 0);
         if (capacity > m_length)
         {
             m_ptr = pim_realloc(m_allocator, m_ptr, sizeof(T) * capacity);
         }
     }
 
+    void Resize(i32 newLength)
+    {
+        ASSERT(newLength >= 0);
+        Reserve(newLength);
+        T *const pim_noalias ptr = m_ptr;
+        const i32 oldLength = m_length;
+        for (i32 i = oldLength - 1; i >= newLength; --i)
+        {
+            ptr[i].~T();
+        }
+        for (i32 i = oldLength - 1; i < newLength; ++i)
+        {
+            new (ptr + i) T();
+        }
+        m_length = newLength;
+    }
+
     T& Grow()
     {
-        const i32 index = m_length;
-        Reserve(index + 1);
-        m_length = index + 1;
+        i32 b = m_length++;
+        m_ptr = pim_realloc(m_allocator, m_ptr, sizeof(T) * (b + 1));
         T *const pim_noalias ptr = m_ptr;
-        new (ptr + index)T();
-        return ptr[index];
+        new (ptr + b) T();
+        return ptr[b];
+    }
+
+    i32 Find(const T& key) const
+    {
+        const i32 length = m_length;
+        T const *const pim_noalias ptr = m_ptr;
+        for (i32 i = length - 1; i >= 0; --i)
+        {
+            if (ptr[i] == key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    bool Contains(const T& key) const { return Find(key) >= 0; }
+
+    bool FindAdd(const T& key)
+    {
+        if (!Contains(key))
+        {
+            Grow() = key;
+            return true;
+        }
+        return false;
+    }
+
+    bool FindRemove(const T& key)
+    {
+        i32 index = Find(key);
+        if (index >= 0)
+        {
+            Remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    void Sort()
+    {
+        QuickSort(m_ptr, m_length);
     }
 };
---
+++ src/containers/dict.hpp
@@ -0,0 +1,441 @@
+#pragma once
+
+#include "common/macro.h"
+#include "allocator/allocator.h"
+#include "common/fnv1a.h"
+#include "containers/hash_util.h"
+#include <new>
+
+template<typename T>
+static u32 HashOf(const T& item)
+{
+    return hashutil_hash(&item, sizeof(T));
+}
+
+template<typename K, typename V>
+class Dict final
+{
+private:
+    i32 m_width;
+    i32 m_count;
+    u32* m_hashes;
+    K* m_keys;
+    V* m_values;
+    EAlloc m_allocator;
+
+public:
+    i32 size() const { return m_count; }
+    i32 capacity() const { return m_width; }
+
+    explicit Dict(i32 capacity = 0, EAlloc allocator = EAlloc_Perm)
+    {
+        m_hashes = NULL;
+        m_keys = NULL;
+        m_values = NULL;
+        m_width = 0;
+        m_count = 0;
+        m_allocator = allocator;
+        if (capacity > 0)
+        {
+            Reserve(capacity);
+        }
+    }
+
+    ~Dict()
+    {
+        Reset();
+    }
+
+    explicit Dict(const Dict& other)
+    {
+        m_hashes = NULL;
+        m_keys = NULL;
+        m_values = NULL;
+        m_width = 0;
+        m_count = 0;
+        m_allocator = other.m_allocator;
+        const u32 width = other.m_width;
+        u32 const *const pim_noalias otherHashes = other.m_hashes;
+        K const *const pim_noalias otherKeys = other.m_keys;
+        V const *const pim_noalias otherValues = other.m_values;
+        Reserve(other.m_count);
+        for (u32 i = 0; i < width; ++i)
+        {
+            if (hashutil_valid(otherHashes[i]))
+            {
+                AddCopy(otherKeys[i], otherValues[i]);
+            }
+        }
+    }
+    Dict& operator=(const Dict& other)
+    {
+        Reset();
+
+        const u32 width = other.m_width;
+        u32 const *const pim_noalias otherHashes = other.m_hashes;
+        K const *const pim_noalias otherKeys = other.m_keys;
+        V const *const pim_noalias otherValues = other.m_values;
+        Reserve(other.m_count);
+        for (u32 i = 0; i < width; ++i)
+        {
+            if (hashutil_valid(otherHashes[i]))
+            {
+                AddCopy(otherKeys[i], otherValues[i]);
+            }
+        }
+        return *this;
+    }
+
+    explicit Dict(Dict&& other)
+    {
+        memcpy(this, &other, sizeof(*this));
+        memset(&other, 0, sizeof(*this));
+        other.m_allocator = m_allocator;
+    }
+    Dict& operator=(Dict&& other)
+    {
+        Reset();
+        memcpy(this, &other, sizeof(*this));
+        memset(&other, 0, sizeof(*this));
+        other.m_allocator = m_allocator;
+        return *this;
+    }
+
+    void Clear()
+    {
+        u32 *const pim_noalias hashes = m_hashes;
+        K *const pim_noalias keys = m_keys;
+        V *const pim_noalias values = m_values;
+        const u32 width = m_width;
+        for (u32 i = 0; i < width; ++i)
+        {
+            if (hashutil_valid(hashes[i]))
+            {
+                hashes[i] = 0;
+                keys[i].~K();
+                values[i].~V();
+            }
+        }
+        m_count = 0;
+    }
+
+    void Reset()
+    {
+        Clear();
+        pim_free(m_hashes);
+        pim_free(m_keys);
+        pim_free(m_values);
+        m_keys = NULL;
+        m_hashes = NULL;
+        m_values = NULL;
+        m_width = 0;
+    }
+
+    void Reserve(i32 count)
+    {
+        ASSERT(count >= 0);
+        count = count > 16 ? count : 16;
+        const u32 newWidth = NextPow2((u32)count) * 2;
+        const u32 oldWidth = m_width;
+        if (newWidth <= oldWidth)
+        {
+            return;
+        }
+
+        u32 *const pim_noalias oldHashes = m_hashes;
+        K *const pim_noalias oldKeys = m_keys;
+        V *const pim_noalias oldValues = m_values;
+
+        const EAlloc allocator = m_allocator;
+        u32 *const pim_noalias newHashes = pim_calloc(allocator, sizeof(newHashes[0]) * newWidth);
+        K *const pim_noalias newKeys = pim_calloc(allocator, sizeof(newKeys[0]) * newWidth);
+        V *const pim_noalias newValues = pim_calloc(allocator, sizeof(newValues[0]) * newWidth);
+
+        const u32 newMask = newWidth - 1;
+        for (u32 i = 0; i < oldWidth;)
+        {
+            if (hashutil_valid(oldHashes[i]))
+            {
+                u32 j = hash;
+                while (true)
+                {
+                    j &= newMask;
+                    if (!newHashes[j])
+                    {
+                        newHashes[j] = oldHashes[i];
+                        memcpy(newKeys + j, oldKeys + i, sizeof(K));
+                        memcpy(newValues + j, oldValues + i, sizeof(V));
+                        goto next;
+                    }
+                    ++j;
+                }
+            }
+        next:
+            ++i;
+        }
+
+        pim_free(oldHashes);
+        pim_free(oldKeys);
+        pim_free(oldValues);
+
+        m_width = newWidth;
+        m_hashes = newHashes;
+        m_keys = newKeys;
+        m_values = newValues;
+    }
+
+    bool Contains(const K& key) const
+    {
+        return Find(key) >= 0;
+    }
+
+    V* Get(const K& key)
+    {
+        i32 i = Find(key);
+        if (i >= 0)
+        {
+            return m_values + i;
+        }
+        return NULL;
+    }
+
+    bool SetMove(const K& key, V&& value)
+    {
+        i32 i = Find(key);
+        if (i >= 0)
+        {
+            m_values[i] = (V&&)value;
+            return true;
+        }
+        return false;
+    }
+    bool SetCopy(const K& key, const V& value)
+    {
+        i32 i = Find(key);
+        if (i >= 0)
+        {
+            m_values[i] = value;
+            return true;
+        }
+        return false;
+    }
+
+    bool AddMove(const K& key, V&& value)
+    {
+        if (Contains(key))
+        {
+            return false;
+        }
+        Reserve(++m_count);
+        u32 keyHash;
+        i32 i = FindInsertion(key, keyHash);
+        hashes[i] = keyHash;
+        new (keys + i) K(key);
+        new (values + i) V((V&&)value);
+        return true;
+    }
+    bool AddCopy(const K& key, const V& value)
+    {
+        if (Contains(key))
+        {
+            return false;
+        }
+        Reserve(++m_count);
+        u32 keyHash;
+        i32 i = FindInsertion(key, keyHash);
+        hashes[i] = keyHash;
+        new (keys + i) K(key);
+        new (values + i) V(value);
+        return true;
+    }
+
+    bool Remove(const K& key, V* valueOut = NULL)
+    {
+        i32 i = Find(key);
+        if (i < 0)
+        {
+            return false;
+        }
+
+        if (valueOut)
+        {
+            *valueOut = (V&&)m_values[i];
+        }
+
+        m_hashes[i] |= hashutil_tomb_mask;
+        m_keys[i].~K();
+        m_values[i].~V();
+
+        --m_count;
+        return true;
+    }
+
+    V& operator[](const K& key)
+    {
+        i32 i = Find(key);
+        if (i >= 0)
+        {
+            return m_values[i];
+        }
+
+        Reserve(++m_count);
+
+        u32 keyHash;
+        i = FindInsertion(key, keyHash);
+        m_hashes[i] = keyHash;
+        new (keys + i) K(key);
+        new (values + i) V();
+        return values[i];
+    }
+
+    // ------------------------------------------------------------------------
+
+    struct Pair final
+    {
+        const K& key;
+        V& value;
+    };
+    class iterator final
+    {
+    private:
+        i32 m_index;
+        i32 m_width;
+        u32 const *const pim_noalias m_hashes;
+        K const *const pim_noalias m_keys;
+        V *const pim_noalias m_values;
+    public:
+        iterator(Dict& dict, i32 index)
+        {
+            m_index = index;
+            m_width = dict.m_width;
+            m_hashes = dict.m_hashes;
+            m_keys = dict.m_keys;
+            m_values = dict.m_values;
+            m_index = Iterate(m_hashes, m_width, m_index);
+        }
+        bool operator!=(const iterator&) const { return m_index < m_width; }
+        iterator& operator++()
+        {
+            m_index = Iterate(m_hashes, m_width, m_index + 1);
+            return *this;
+        }
+        Pair operator*()
+        {
+            i32 i = m_index;
+            ASSERT(i < m_width);
+            ASSERT(hashutil_valid(m_hashes[i]));
+            return Pair{ m_keys[i], m_values[i] };
+        }
+    };
+    iterator begin() { return iterator(*this, 0); }
+    iterator end() { return iterator(*this, m_width); }
+
+    // ------------------------------------------------------------------------
+
+    struct ConstPair final
+    {
+        const K& key;
+        const V& value;
+    };
+    class const_iterator final
+    {
+    private:
+        i32 m_index;
+        i32 m_width;
+        u32 const *const pim_noalias m_hashes;
+        K const *const pim_noalias m_keys;
+        V const *const pim_noalias m_values;
+    public:
+        const_iterator(const Dict& dict, i32 index)
+        {
+            m_index = index;
+            m_width = dict.m_width;
+            m_hashes = dict.m_hashes;
+            m_keys = dict.m_keys;
+            m_values = dict.m_values;
+            m_index = Iterate(m_hashes, m_width, m_index);
+        }
+        bool operator!=(const const_iterator&) const { return m_index < m_width; }
+        const_iterator& operator++()
+        {
+            m_index = Iterate(m_hashes, m_width, m_index + 1);
+            return *this;
+        }
+        ConstPair operator*()
+        {
+            i32 i = m_index;
+            ASSERT(i < m_width);
+            ASSERT(hashutil_valid(m_hashes[i]));
+            return ConstPair{ m_keys[i], m_values[i] };
+        }
+    };
+    const_iterator begin() const { return const_iterator(*this, 0); }
+    const_iterator end() const { return const_iterator(*this, m_width); }
+
+    // ------------------------------------------------------------------------
+
+private:
+
+    static i32 Iterate(u32 const *const pim_noalias hashes, i32 width, i32 index)
+    {
+        while (index < width)
+        {
+            if (hashutil_valid(hashes[index]))
+            {
+                break;
+            }
+            ++index;
+        }
+        return index;
+    }
+
+    i32 Find(const K& key) const
+    {
+        const u32 keyHash = HashOf(key);
+
+        u32 width = m_width;
+        const u32 mask = width - 1;
+        u32 const *const pim_noalias hashes = m_hashes;
+        K const *const pim_noalias keys = m_keys;
+
+        u32 j = keyHash;
+        while (width--)
+        {
+            j &= mask;
+            const u32 jHash = hashes[j];
+            if (hashutil_empty(jHash))
+            {
+                break;
+            }
+            if (jHash == keyHash)
+            {
+                if (key == keys[j])
+                {
+                    return (i32)j;
+                }
+            }
+            ++j;
+        }
+
+        return -1;
+    }
+
+    i32 FindInsertion(const K& key, u32& hashOut) const
+    {
+        const u32 mask = m_width - 1;
+        const u32 keyHash = HashOf(key);
+        hashOut = keyHash;
+        u32 const *const pim_noalias hashes = m_hashes;
+        u32 j = keyHash;
+        while (true)
+        {
+            j &= mask;
+            if (hashutil_empty_or_tomb(hashes[j]))
+            {
+                return (i32)j;
+            }
+            ++j;
+        }
+    }
+};
+
