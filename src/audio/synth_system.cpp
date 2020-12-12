0b7cadd2e138e5f1bb734b5b7b1bbb4363a65202
Kara <lh0xfb@gmail.com>
2020-12-11 22:51:40 -0800

    quick and dirty little synth

--- src/audio/synth_system.cpp
+++ src/audio/synth_system.cpp
@@ -7,25 +7,122 @@
 #include "math/float4.hpp"
 #include "audio/modular/board.hpp"
 
+static constexpr double kSamplesPerSecond = 44100.0;
+static constexpr double kSecondsPerSample = 1.0 / kSamplesPerSecond;
+
 static Modular::Board ms_board;
 static float ms_volume = 0.0f;
 static bool ms_capture;
-static float ms_freq;
-static float ms_dphase;
-static float ms_phase;
+static double ms_freq;
 static i32 ms_octave = 4;
 static i32 ms_note = 0;
 
-inline float MidiToHertz(i32 note)
+static double NoteToHertz(double note)
 {
-    float a = (note - 69) * (1.0f / 12.0f);
-    return 440.0f * exp2f(a);
+    double a = (note - 69.0) * (1.0 / 12.0);
+    return 440.0f * exp2(a);
 }
 
-inline float HertzToDeltaPhase(float hz)
+static double HertzToDeltaPhase(double hz)
 {
-    return hz * (1.0f / 44100.0f);
+    return hz * (1.0 / 44100.0);
 }
+
+static double SineWave(double phase)
+{
+    return sin(phase * kTau);
+}
+
+static double Impulse(double phase, double rcpScale)
+{
+    double t = phase * rcpScale;
+    return t * exp(1.0 - t);
+}
+
+struct Oscillator
+{
+    double m_phase;
+    double m_dphase;
+    i32 m_octave;
+    i32 m_semitone;
+    float m_fine;
+    double m_offset;
+
+    void OnGui()
+    {
+        ImGui::PushID(this);
+        ImGui::SliderInt("Octave", &m_octave, -4, 4);
+        ImGui::SliderInt("Semitone", &m_semitone, -12, 12);
+        ImGui::SliderFloat("Fine", &m_fine, -1.0f, 1.0f);
+        ImGui::PopID();
+        double note = m_octave * 12 + m_semitone + m_fine;
+        double hz = NoteToHertz(note);
+        m_offset = hz;
+    }
+    void SetFreq(double hz)
+    {
+        m_dphase = HertzToDeltaPhase(hz + m_offset);
+    }
+    double Sample(double phaseMod)
+    {
+        m_phase = fmod(m_phase + m_dphase + phaseMod, 1.0f);
+        return SineWave(m_phase);
+    }
+};
+
+struct Envelope
+{
+    double m_phase;
+    double m_rcpScale;
+
+    void OnGui()
+    {
+        ImGui::PushID(this);
+        float scale = 1.0f / f1_max(kEpsilon, (float)m_rcpScale);
+        ImGui::SliderFloat("Scale", &scale, kMilli, 1.0f);
+        m_rcpScale = 1.0 / f1_max(kEpsilon, scale);
+        ImGui::PopID();
+    }
+    void Trigger()
+    {
+        m_phase = 0.0;
+    }
+    double Sample()
+    {
+        double value = Impulse(m_phase, m_rcpScale);
+        m_phase += kSecondsPerSample;
+        return value;
+    }
+};
+
+struct Voice
+{
+    Oscillator m_oscillator;
+    Envelope m_envelope;
+    float m_volume;
+
+    void OnGui()
+    {
+        ImGui::PushID(this);
+        ImGui::SliderFloat("Volume", &m_volume, 0.0f, 1.0f);
+        m_oscillator.OnGui();
+        m_envelope.OnGui();
+        ImGui::PopID();
+    }
+    void SetVolume(float volume)
+    {
+        m_volume = volume;
+    }
+    double Sample(double phaseMod)
+    {
+        double value = m_volume * m_oscillator.Sample(phaseMod);
+        double envelope = m_envelope.Sample();
+        return value * envelope;
+    }
+};
+
+static Voice ms_mod;
+static Voice ms_primary;
 
 void SynthSys::Init()
 {
@@ -39,6 +136,7 @@ void SynthSys::Update()
 
     //if (ms_capture)
     {
+        bool gate = false;
         if (input_keydown(KeyCode_Z))
         {
             --ms_octave;
@@ -52,10 +150,17 @@ void SynthSys::Update()
             if (input_keydown((KeyCode)(KeyCode_0 + i)))
             {
                 ms_note = i;
+                gate = true;
             }
         }
-        ms_freq = MidiToHertz(ms_octave * 12 + ms_note);
-        ms_dphase = HertzToDeltaPhase(ms_freq);
+        ms_freq = NoteToHertz(ms_octave * 12.0 + (double)ms_note);
+        ms_primary.m_oscillator.SetFreq(ms_freq);
+        ms_mod.m_oscillator.SetFreq(ms_freq);
+        if (gate)
+        {
+            ms_primary.m_envelope.Trigger();
+            ms_mod.m_envelope.Trigger();
+        }
     }
 }
 
@@ -72,6 +177,8 @@ void SynthSys::OnGui()
     {
         ImGui::Checkbox("Use Keyboard", &ms_capture);
         ImGui::SliderFloat("Volume", &ms_volume, 0.0f, 1.0f);
+        ms_primary.OnGui();
+        ms_mod.OnGui();
         ImGui::TreePop();
     }
 }
@@ -79,21 +186,14 @@ void SynthSys::OnGui()
 void SynthSys::OnSample(float *const pim_noalias buffer, i32 frames, i32 channels)
 {
     ASSERT(channels == 2);
-    const i32 spp = 8;
-    const float dphase = ms_dphase / spp;
-    const float volume = ms_volume;
-    float phase = ms_phase;
+    const double volume = ms_volume;
     for (i32 i = 0; i < frames; ++i)
     {
-        float value = 0.0f;
-        for (i32 j = 0; j < spp; ++j)
-        {
-            phase = fmodf(phase + dphase, 1.0f);
-            value += sinf(phase * kTau);
-        }
-        value *= volume / spp;
-        buffer[i * 2 + 0] = value;
-        buffer[i * 2 + 1] = value;
+        double pm = ms_mod.Sample(0.0);
+        double value = ms_primary.Sample(pm);
+        value *= volume;
+        float value32 = (float)value;
+        buffer[i * 2 + 0] = value32;
+        buffer[i * 2 + 1] = value32;
     }
-    ms_phase = phase;
 }
