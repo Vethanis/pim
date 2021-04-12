b71146d3c50e35a17053aeba3a27b6b4060073e6
Kara <lh0xfb@gmail.com>
2021-04-12 02:47:53 -0700

    profiler improvements

--- src/common/profiler.c
+++ src/common/profiler.c
@@ -17,7 +17,7 @@
 #define kNodeLimit (1024)
 
 // eventually the call stack will run out in the Gui
-#define kDepthLimit (50)
+#define kDepthLimit (20)
 
 // ----------------------------------------------------------------------------
 
@@ -30,7 +30,8 @@ typedef struct node_s
     struct node_s* sibling;
     u64 begin;
     u64 end;
-    u32 hash;
+    u64 hash;
+    i32 depth;
 } node_t;
 
 typedef struct stat_s
@@ -39,41 +40,43 @@ typedef struct stat_s
     double variance;
 } stat_t;
 
+typedef struct ctx_s
+{
+    node_t prevroot;
+    node_t root;
+    node_t* current;
+    u32 frame;
+    u32 count;
+    i32 depth;
+} ctx_t;
+
 // ----------------------------------------------------------------------------
 
-static void VisitClr(node_t* node, i32 depth);
-static void VisitSum(node_t* node, i32 depth);
-static void VisitGui(const node_t* node, i32 depth);
+static void VisitClr(node_t* node);
+static void VisitSum(node_t* node);
+static void VisitGui(const node_t* node);
 static stat_t GetNodeStats(const node_t* node);
 static stat_t UpdateNodeStats(const node_t* node);
 
 // ----------------------------------------------------------------------------
 
-static u32 ms_frame[kMaxThreads];
-static u32 ms_count[kMaxThreads];
-static node_t ms_prevroots[kMaxThreads];
-static node_t ms_roots[kMaxThreads];
-static node_t* ms_top[kMaxThreads];
+static ctx_t ms_ctx[kMaxThreads];
+static ctx_t* GetContext(void) { return &ms_ctx[Task_ThreadId()]; }
 
-static Dict ms_stats;
+static Dict ms_stats =
+{
+    .keySize = sizeof(u64),
+    .valueSize = sizeof(stat_t),
+};
 static i32 ms_avgWindow = 20;
 static bool ms_progressive;
 
 // ----------------------------------------------------------------------------
 
-static void EnsureDict(void)
-{
-    if (!ms_stats.valueSize)
-    {
-        Dict_New(&ms_stats, sizeof(u32), sizeof(stat_t), EAlloc_Perm);
-    }
-}
-
 ProfileMark(pm_gui, ProfileSys_Gui)
 void ProfileSys_Gui(bool* pEnabled)
 {
     ProfileBegin(pm_gui);
-    EnsureDict();
 
     if (igBegin("Profiler", pEnabled, 0))
     {
@@ -91,29 +94,32 @@ void ProfileSys_Gui(bool* pEnabled)
             igExSliderInt("Window", &ms_avgWindow, 1, 1000);
         }
 
-        node_t* root = ms_prevroots[0].fchild;
+        ctx_t* ctx = GetContext();
+        node_t* root = ctx->prevroot.fchild;
 
         igSeparator();
 
-        VisitClr(root, 0);
-        VisitSum(root, 0);
+        VisitClr(root);
+        VisitSum(root);
 
         ImVec2 region;
         igGetContentRegionAvail(&region);
-        igExColumns(4);
-        igSetColumnWidth(0, region.x * 0.6f);
-        igSetColumnWidth(1, region.x * 0.13333333333f);
-        igSetColumnWidth(2, region.x * 0.13333333333f);
-        igSetColumnWidth(3, region.x * 0.13333333333f);
+        igExColumns(5);
+        igSetColumnWidth(0, region.x * 0.55f);
+        igSetColumnWidth(1, region.x * (0.45f / 4.0f));
+        igSetColumnWidth(2, region.x * (0.45f / 4.0f));
+        igSetColumnWidth(3, region.x * (0.45f / 4.0f));
+        igSetColumnWidth(4, region.x * (0.45f / 4.0f));
         {
             igText("Name"); igNextColumn();
             igText("Milliseconds"); igNextColumn();
             igText("Std Dev."); igNextColumn();
-            igText("Percent"); igNextColumn();
+            igText("%%Root"); igNextColumn();
+            igText("%%Parent"); igNextColumn();
 
             igSeparator();
 
-            VisitGui(root, 0);
+            VisitGui(root);
         }
         igExColumns(1);
     }
@@ -126,111 +132,102 @@ void ProfileSys_Gui(bool* pEnabled)
 
 void _ProfileBegin(ProfMark *const mark)
 {
-    ASSERT(mark);
-    const i32 tid = Task_ThreadId();
+    ASSERT(mark && mark->name);
+    ctx_t *const pim_noalias ctx = GetContext();
+
     const u32 frame = Time_FrameCount();
-
-    if (frame != ms_frame[tid])
+    if (frame != ctx->frame)
     {
-        ms_prevroots[tid] = ms_roots[tid];
-        ms_roots[tid] = (node_t){ 0 };
-        ms_frame[tid] = frame;
-        ms_top[tid] = NULL;
-        ms_count[tid] = 0;
+        ctx->frame = frame;
+        ctx->count = 0;
+        ctx->prevroot = ctx->root;
+        ctx->root = (node_t) { .hash = Fnv64Bias };
+        ctx->current = &ctx->root;
+        ctx->depth = 0;
     }
 
-    if ((++ms_count[tid]) > kNodeLimit)
+    ctx->count++;
+    ctx->depth++;
+    if ((ctx->count < kNodeLimit) && (ctx->depth < kDepthLimit))
     {
-        return;
-    }
+        node_t *const parent = ctx->current ? ctx->current : &ctx->root;
+        node_t *const pim_noalias next = Temp_Calloc(sizeof(*next));
+        next->mark = mark;
+        next->parent = parent;
+        next->depth = ctx->depth;
+        u64 prevHash = parent->hash;
+        node_t *const sibling = parent->lchild;
+        if (sibling)
+        {
+            sibling->sibling = next;
+            prevHash = sibling->hash;
+        }
+        else
+        {
+            parent->fchild = next;
+        }
+        parent->lchild = next;
+        next->hash = Fnv64Qword((u64)mark, prevHash) | 1;
+        ctx->current = next;
 
-    node_t* top = ms_top[tid];
-    if (!top)
-    {
-        top = &ms_roots[tid];
+        next->begin = Time_Now();
     }
-
-    node_t *const next = Temp_Calloc(sizeof(*next));
-    next->mark = mark;
-    next->parent = top;
-    if (top->lchild)
-    {
-        top->lchild->sibling = next;
-    }
-    else
-    {
-        top->fchild = next;
-    }
-    top->lchild = next;
-    ms_top[tid] = next;
-
-    next->begin = Time_Now();
 }
 
 void _ProfileEnd(ProfMark *const mark)
 {
     const u64 end = Time_Now();
 
-    ASSERT(mark);
+    ASSERT(mark && mark->name);
 
-    const i32 tid = Task_ThreadId();
-    if (ms_count[tid] >= kNodeLimit)
+    ctx_t *const pim_noalias ctx = GetContext();
+    if ((ctx->count < kNodeLimit) && (ctx->depth < kDepthLimit))
     {
-        return;
+        node_t *const current = ctx->current;
+
+        ASSERT(current);
+        ASSERT(current->mark == mark);
+        ASSERT(current->depth == ctx->depth);
+        ASSERT(current->parent);
+        ASSERT(current->begin);
+        ASSERT(!current->end);
+        ASSERT(current->hash);
+        ASSERT(Time_FrameCount() == ctx->frame);
+
+        current->end = end;
+        ctx->current = current->parent;
     }
-    node_t *const top = ms_top[tid];
 
-    ASSERT(top);
-    ASSERT(top->mark == mark);
-    ASSERT(top->parent);
-    ASSERT(top->begin);
-    ASSERT(top->end == 0);
-    ASSERT(Time_FrameCount() == ms_frame[tid]);
-
-    top->end = end;
-    ms_top[tid] = top->parent;
+    ctx->depth--;
 }
 
 // ----------------------------------------------------------------------------
 
-static double Lerp64(double a, double b, double t)
+pim_inline double VEC_CALL f64_lerp(double a, double b, double t)
 {
     return a + (b - a) * t;
 }
 
-static void VisitClr(node_t* node, i32 depth)
+static void VisitClr(node_t* node)
 {
-    if (depth > kDepthLimit)
-    {
-        return;
-    }
     while (node)
     {
         ProfMark *const mark = node->mark;
         ASSERT(mark);
         mark->calls = 0;
         mark->sum = 0;
-        u32 hash = mark->hash;
-        if (hash == 0)
-        {
-            hash = HashStr(mark->name);
-            mark->hash = hash;
-        }
-        node->hash = hash;
 
-        VisitClr(node->fchild, depth + 1);
+        if (node->depth < kDepthLimit)
+        {
+            VisitClr(node->fchild);
+        }
 
         node = node->sibling;
     }
 }
 
-static void VisitSum(node_t* node, i32 depth)
+static void VisitSum(node_t* node)
 {
-    if (depth > kDepthLimit)
-    {
-        return;
-    }
-
     while (node)
     {
         ProfMark *const mark = node->mark;
@@ -238,28 +235,17 @@ static void VisitSum(node_t* node, i32 depth)
         mark->calls += 1;
         mark->sum += node->end - node->begin;
 
-        u32 hash = node->hash;
-        ASSERT(node->parent);
-        hash = Fnv32Dword(node->parent->hash, hash);
-        if (node->sibling)
+        if (node->depth < kDepthLimit)
         {
-            hash = Fnv32Dword(node->sibling->hash, hash);
+            VisitSum(node->fchild);
         }
-        node->hash = hash;
-
-        VisitSum(node->fchild, depth + 1);
 
         node = node->sibling;
     }
 }
 
-static void VisitGui(const node_t* node, i32 depth)
+static void VisitGui(const node_t* node)
 {
-    if (depth > kDepthLimit)
-    {
-        return;
-    }
-
     while (node)
     {
         ProfMark const *const mark = node->mark;
@@ -268,29 +254,39 @@ static void VisitGui(const node_t* node, i32 depth)
         ASSERT(name);
 
         stat_t st = UpdateNodeStats(node);
-
-        double pct = 0.0;
-        const node_t* root = ms_prevroots[0].fchild;
-        ASSERT(root);
-
-        stat_t rootst = GetNodeStats(root);
-        if (rootst.mean > 0.0)
-        {
-            pct = 100.0 * (st.mean / rootst.mean);
-        }
-
         if (st.mean > 0.0001)
         {
+            double parentPct = 0.0;
+            if (node->parent)
+            {
+                stat_t parentStat = GetNodeStats(node->parent);
+                if (parentStat.mean > 0.0)
+                {
+                    parentPct = 100.0 * (st.mean / parentStat.mean);
+                }
+            }
+            double rootPct = 0.0;
+            const node_t* const root = GetContext()->prevroot.fchild;
+            if (root)
+            {
+                stat_t rootStat = GetNodeStats(root);
+                if (rootStat.mean > 0.0)
+                {
+                    rootPct = 100.0 * (st.mean / rootStat.mean);
+                }
+            }
+
             igText("%s", name); igNextColumn();
             igText("%03.4f", st.mean); igNextColumn();
             igText("%03.4f", sqrt(st.variance)); igNextColumn();
-            igText("%4.1f%%", pct); igNextColumn();
+            igText("%4.1f%%", rootPct); igNextColumn();
+            igText("%4.1f%%", parentPct); igNextColumn();
 
-            char key[32];
-            SPrintf(ARGS(key), "%x", node->hash);
-
-            igTreePushStr(key);
-            VisitGui(node->fchild, depth + 1);
+            igTreePushPtr((void*)(node->hash));
+            if (node->depth < kDepthLimit)
+            {
+                VisitGui(node->fchild);
+            }
             igTreePop();
         }
 
@@ -301,7 +297,6 @@ static void VisitGui(const node_t* node, i32 depth)
 static stat_t GetNodeStats(const node_t* node)
 {
     ASSERT(node);
-    ASSERT(node->mark);
     ASSERT(node->hash);
     stat_t st = { 0 };
     Dict_Get(&ms_stats, &node->hash, &st);
@@ -318,10 +313,10 @@ static stat_t UpdateNodeStats(const node_t* node)
     if (Dict_Get(&ms_stats, &node->hash, &st))
     {
         const double alpha = 1.0 / ms_avgWindow;
-        double m0 = st.mean;
-        double m1 = Lerp64(m0, x, alpha);
-        st.variance = Lerp64(st.variance, (x - m0) * (x - m1), alpha);
-        st.mean = m1;
+        double mean = st.mean;
+        double variance = (x - mean) * (x - mean);
+        st.variance = f64_lerp(st.variance, variance, alpha);
+        st.mean = f64_lerp(mean, x, alpha);
         Dict_Set(&ms_stats, &node->hash, &st);
     }
     else
