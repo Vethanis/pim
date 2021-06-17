c44ec10f34a1f41de1f3e3308ece20f7a9ba9434
Vivianne Langdon <puttabutta@gmail.com>
2021-06-17 12:10:46 -0700

    Support custom source path for importer

--- src/tools/datacopy.sh
+++ src/tools/datacopy.sh
@@ -5,6 +5,13 @@ pushd ../../
 SRCPATH="C:/Program Files (x86)/Steam/steamapps/common/Quake/Id1"
 DSTPATH="data/id1"
 
+echo -n "Please specify your Quake Id1 path [${SRCPATH}]: "
+read -r NEWSRCPATH
+if [ ! -z "$NEWSRCPATH" ]
+  then
+    SRCPATH=${NEWSRCPATH//\\//}
+fi
+
 mkdir -p "${DSTPATH}"
 
 cp "${SRCPATH}/config.cfg" "${DSTPATH}/config.cfg"
