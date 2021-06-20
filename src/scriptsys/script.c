e06c0b93e4312ae6d55384cf7456b949bde54255
Vivianne Langdon <puttabutta@gmail.com>
2021-06-19 17:27:39 -0700

    Load init script, and actually clean up
    Also move script dir into root/script/

--- src/scriptsys/script.c
+++ src/scriptsys/script.c
@@ -7,5 +7,10 @@ void ScriptSys_Init()
 	ms_luastate = luaL_newstate();
 	luaL_openlibs(ms_luastate);
 
-	luaL_dostring(ms_luastate, "print(\"Hello World!\", \"2 * 2 is \", 2*2)");
+	luaL_dofile(ms_luastate, SCRIPT_DIR "init.lua");
+}
+
+void ScriptSys_Shutdown()
+{
+	lua_close(ms_luastate);
 }
