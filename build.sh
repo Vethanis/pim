6bb32ab1cdc3645f9fd75e834f2aa443d892d07a
Vivianne Langdon <puttabutta@gmail.com>
2022-06-20 10:42:27 -0700

    add scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p build/Debug
+mkdir build/Release
+
+pushd build/Debug
+cmake -DCMAKE_BUILD_TYPE=DEBUG ../../
+popd
+
+pushd build/Release
+cmake -DCMAKE_BUILD_TYPE=RELEASE ../../
+popd
+
+cmake --build build/Debug
+cmake --build build/Release
