5f42ea345bb0c6237a457260ddac4305ff56355e
Vivianne Langdon <puttabutta@gmail.com>
2022-06-20 20:54:10 -0700

    define posix methods on windows if not defined, except for mkdir which needs another arg in linux

--- src/io/dir.c
+++ src/io/dir.c
@@ -22,21 +22,27 @@ pim_inline i32 IsZero(i32 x)
 #include <direct.h>
 #include <io.h>
 
-#define pim_getcwd(d, s) _getcwd(d, s)
-#define pim_chdir(p) _chdir(p)
+#ifndef getcwd
+#   define getcwd(d, s) _getcwd(d, s)
+#endif // getcwd
+#ifndef chdir
+#   define chdir(p) _chdir(p)
+#endif // chdir
+#ifndef rmdir
+#   define rmdir(p) _rmdir(p)
+#endif // rmdir
+#ifndef chmod
+#   define chmod(p, f) _chmod(p, f)
+#endif // chmod
+
+// need special mkdir as linux has a second arg for permissions
 #define pim_mkdir(p) _mkdir(p)
-#define pim_rmdir(p) _rmdir(p)
-#define pim_chmod(p, f) _chmod(p, f)
 
 #else
 #include <unistd.h>
 #include <sys/stat.h>
 
-#define pim_getcwd(d, s) getcwd(d, s)
-#define pim_chdir(p) chdir(p)
 #define pim_mkdir(p) mkdir(p, 0755)
-#define pim_rmdir(p) rmdir(p)
-#define pim_chmod(p, f) chmod(p, f)
 
 #endif // PLAT_WINDOWS
 
@@ -44,13 +50,13 @@ bool IO_GetCwd(char* dst, i32 size)
 {
     ASSERT(dst);
     ASSERT(size > 0);
-    return NotNull(pim_getcwd(dst, size - 1)) != NULL;
+    return NotNull(getcwd(dst, size - 1)) != NULL;
 }
 
 bool IO_ChDir(const char* path)
 {
     ASSERT(path);
-    return IsZero(pim_chdir(path)) == 0;
+    return IsZero(chdir(path)) == 0;
 }
 
 bool IO_MkDir(const char* path)
@@ -62,11 +68,11 @@ bool IO_MkDir(const char* path)
 bool IO_RmDir(const char* path)
 {
     ASSERT(path);
-    return IsZero(pim_rmdir(path)) == 0;
+    return IsZero(rmdir(path)) == 0;
 }
 
 bool IO_ChMod(const char* path, i32 flags)
 {
     ASSERT(path);
-    return IsZero(pim_chmod(path, flags)) == 0;
+    return IsZero(chmod(path, flags)) == 0;
 }
