41c279a82846ad08e1785a957bf1c4a48a8353b7
Kara <lh0xfb@gmail.com>
2022-06-22 00:29:25 -0700

    abstract away more of the io platform differences

--- src/io/env.c
+++ src/io/env.c
@@ -5,7 +5,7 @@
 
 #if PLAT_WINDOWS
 #ifndef putenv
-#   define putenv(p)   _putenv(p)
+#   define putenv(p)   _putenv((p))
 #endif // putenv
 #endif // PLAT_WINDOWS
 
@@ -21,51 +21,10 @@ static i32 IsZero(i32 x)
     return x;
 }
 
-bool Env_Search(const char* filename, const char* varname, char* dst)
-{
-    ASSERT(filename);
-    ASSERT(varname);
-    ASSERT(dst);
-    dst[0] = 0;
-
-    #if PLAT_WINDOWS
-
-    _searchenv(filename, varname, dst);
-
-    #else
-
-    // TODO: unsure how we're meant to safely copy to destination without length.
-    // Currently env_search isn't referenced though.
-    /*
-    struct stat buffer;
-    char* env = getenv("PATH");
-    do
-    {
-        char* p = strchr(env, ':');
-        if (p != NULL && stat(p, &buffer) == 0)
-        {
-            break;
-        }
-        env = p + 1;
-    } 
-    while (p != NULL);
-
-    if (p != null)
-    {
-        StrCpy(dst, ???, p);
-    }
-    */
-
-    #endif // PLAT_WINDOWS
-
-
-    return dst[0] != 0;
-}
-
 const char* Env_Get(const char* varname)
 {
     ASSERT(varname);
-    return (const char*)NotNull(getenv(varname));
+    return NotNull(getenv(varname));
 }
 
 bool Env_Set(const char* varname, const char* value)
